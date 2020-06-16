60d85ee03dcad37937b5de544d73806e6a082a1f
MrWilllian <willian.dev10@gmail.com>
2020-06-16 14:51:19 -0300

    refactor: add Login Container screen

---
+++ WashApp/src/screens/LoginContainer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import TabNavigator from '../../components/TabNavigator';
+
+export default function LoginContainer() {
+  return (
+    <SafeAreaView>
+      <TabNavigator />
+    </SafeAreaView>
+  );
+}

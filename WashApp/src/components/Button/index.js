c5cd1f56f75f47e0b79886532ac7134f9a05be5a
MrWilllian <willian.dev10@gmail.com>
2020-06-16 14:49:34 -0300

    refactor: add Button component

---
+++ WashApp/src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+export default function Button() {
+  return (
+    <TouchableOpacity style={styles.container} />
+  );
+}
---
+++ WashApp/src/components/Button/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../assets/colors';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: colors.accent,
+    position: 'absolute',
+    height: 60,
+    width: 90,
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    bottom: 40,
+    right: 20,
+    
+  }
+});

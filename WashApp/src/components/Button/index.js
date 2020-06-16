d0ebdb8a2693db89fae053751f9b41cbde8b28e4
MrWilllian <willian.dev10@gmail.com>
2020-06-16 15:56:48 -0300

    refactor: add icon to button

--- WashApp/src/components/Button/index.js
+++ WashApp/src/components/Button/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons'
 
 import styles from './styles';
 
 export default function Button() {
   return (
-    <TouchableOpacity style={styles.container} />
+    <TouchableOpacity style={styles.container}>
+      <Icon name="forward" size={40} color={'#FFF'} />
+    </TouchableOpacity>
   );
 }

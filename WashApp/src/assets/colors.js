3bddfd8b6f07f26311dbd01fd61e228cbc3791df
MrWilllian <willian.dev10@gmail.com>
2020-06-16 14:25:58 -0300

    refactor: add colors file

---
+++ WashApp/src/assets/colors.js
@@ -0,0 +1,14 @@
+export default colors = {
+  primary: '#FFFCFC',
+
+  accent: '#FC4F05',
+
+  font: '#181059',
+  
+  cinza1: '#FDFDFD',
+  cinza2: '#C4C4C4',
+  cinza3: '#4E4C63',
+
+  white: '#FFF',
+  black: '#000',
+}

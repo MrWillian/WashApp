a55001cef9d9c769d52b010935c828887281b9ac
MrWilllian <willian.dev10@gmail.com>
2020-06-16 14:50:14 -0300

    refactor: add LoginContainer and Register routes

--- WashApp/src/routes.js
+++ WashApp/src/routes.js
@@ -4,13 +4,17 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
+import LoginContainer from './screens/LoginContainer';
 import Login from './screens/Login';
+import Register from './screens/Register';
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
+        <AppStack.Screen name="LoginContainer" component={LoginContainer} />
         <AppStack.Screen name="Login" component={Login} />
+        <AppStack.Screen name="Register" component={Register} />
       </AppStack.Navigator>
     </NavigationContainer>
   );

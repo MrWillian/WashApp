6229f04cd1ec3ff066f6b736dfcd2f70ca4c98e8
MrWilllian <willian.dev10@gmail.com>
2020-06-16 14:49:20 -0300

    refactor: add Footer component

---
+++ WashApp/src/components/BottomScreen/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../assets/colors';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: colors.cinza2,
+    height: '10%',
+    position: 'relative',
+    marginTop: 'auto',
+  }
+});

7fe1a51014174b884bb5da2ed7f9526f846d5b82
MrWilllian <willian.dev10@gmail.com>
2020-06-17 15:06:08 -0300

    refactor: add Register Company and Home routes

--- WashApp/src/routes.js
+++ WashApp/src/routes.js
@@ -4,17 +4,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 const AppStack = createStackNavigator();
 
-import LoginContainer from './screens/LoginContainer';
 import Login from './screens/Login';
 import Register from './screens/Register';
+import RegisterCompany from './screens/RegisterCompany';
+import Home from './screens/Home';
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator screenOptions={{ headerShown: false }}>
-        <AppStack.Screen name="LoginContainer" component={LoginContainer} />
         <AppStack.Screen name="Login" component={Login} />
         <AppStack.Screen name="Register" component={Register} />
+        <AppStack.Screen name="RegisterCompany" component={RegisterCompany} />
+        <AppStack.Screen name="Home" component={Home} />
       </AppStack.Navigator>
     </NavigationContainer>
   );

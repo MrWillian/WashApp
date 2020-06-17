3d1b4a5c57a1c58e2614389630cf8e429cade625
MrWilllian <willian.dev10@gmail.com>
2020-06-17 14:21:32 -0300

    style: add register company screen style

---
+++ WashApp/src/screens/RegisterCompany/styles.js
@@ -0,0 +1,30 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../assets/colors';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  form: {
+    flex: 1,
+    justifyContent: 'center',
+    margin: 30,
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: colors.font,
+  },
+  label: {
+    fontSize: 18,
+    color: colors.font,
+  },
+  textInput: {
+    borderBottomWidth: 0.3,
+    borderColor: colors.black,
+    color: colors.font,
+    borderRadius: 5
+  },
+});

f9063f1d733e4789db185800c726b41dbb9e4d4f
MrWilllian <willian.dev10@gmail.com>
2020-06-22 19:55:37 -0300

    refactor: add user data

--- WashApp/src/screens/Home/index.js
+++ WashApp/src/screens/Home/index.js
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text } from 'react-native';
 
 import CompanyList from '../../components/CompanyList';
 import BottomScreen from '../../components/BottomScreen';
 import FabButton from '../../components/FabButton';
 
+import { useRoute } from '@react-navigation/native';
+
 import styles from './styles';
 
 export default function Home() {
+  const [user, setUser] = useState({});
+  const route = useRoute();
+
+  useEffect(() => {
+    setUser(route.params.data);
+  }, []);
 
   return (
     <View style={styles.container}>
 
       <View style={styles.contentContainer}>
-        <Text style={styles.title}>Olá...</Text>
+      <Text style={styles.title}>Olá, {user.name}</Text>
         
         <CompanyList />
 

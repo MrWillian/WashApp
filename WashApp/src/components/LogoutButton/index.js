67b1e787232eb129f89d4b1ab632bdbfc49f999b
MrWilllian <willian.dev10@gmail.com>
2020-06-22 20:39:28 -0300

    refactor: add action to logout button component

--- WashApp/src/components/LogoutButton/index.js
+++ WashApp/src/components/LogoutButton/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 import styles from './styles';
 
+import { removeItem } from '../../controllers/StorageController';
+
 export default function LogoutButton() {
+  
+  const navigation = useNavigation();
+
+  async function logout() {
+    await removeItem('userSession');
+    navigation.navigate('Login'); 
+  }
+
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity 
+      style={styles.container}
+      onPress={logout}>
       <Text style={styles.label}>X</Text>
     </TouchableOpacity>
   );

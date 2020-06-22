73df04b345918d4bac0645d33dcb467a92671f52
MrWilllian <willian.dev10@gmail.com>
2020-06-22 19:54:53 -0300

    refactor: get companies from db

--- WashApp/src/components/CompanyList/index.js
+++ WashApp/src/components/CompanyList/index.js
@@ -3,64 +3,20 @@ import { View, FlatList } from 'react-native';
 
 import CompanyItem from '../../components/CompanyItem';
 
+import { allCompanies } from '../../controllers/CompanyController';
+
 import styles from './styles';
 
 export default function CompanyList() {
-  const [companies, setCompanies] = useState('');
-  const listCompany = [
-    {
-      "id": 1,
-      "name": "Lava-jato 1",
-      "value": 25,
-      "cnpj": "42.621.645/0001-91",
-      "tellphone": null,
-      "user_id": 10,
-    },
-    {
-      "id": 2,
-      "name": "Lava-jato 2",
-      "value": 27,
-      "cnpj": "42.621.645/0001-91",
-      "tellphone": null,
-      "user_id": 10,
-    },
-    {
-      "id": 3,
-      "name": "Lava-jato 3",
-      "value": 23,
-      "cnpj": "42.621.645/0001-91",
-      "tellphone": null,
-      "user_id": 10,
-    },
-    {
-      "id": 4,
-      "name": "Lava-jato 4",
-      "value": 22,
-      "cnpj": "42.621.645/0001-91",
-      "tellphone": null,
-      "user_id": 10,
-    },
-    {
-      "id": 5,
-      "name": "Lava-jato 5",
-      "value": 22,
-      "cnpj": "42.621.645/0001-91",
-      "tellphone": null,
-      "user_id": 10,
-    },
-    {
-      "id": 6,
-      "name": "Lava-jato 6",
-      "value": 22,
-      "cnpj": "42.621.645/0001-91",
-      "tellphone": null,
-      "user_id": 10,
-    },
-  ];
+  const [companies, setCompanies] = useState([]);
 
-  useEffect(() => {
-    setCompanies(listCompany);
+  useEffect(() => {   
+    getAllCompanies();
   },[]);
+
+  async function getAllCompanies() {
+    setCompanies(await allCompanies());
+  }
 
   return (
     <View style={styles.container}>
@@ -70,8 +26,7 @@ export default function CompanyList() {
         showsVerticalScrollIndicator={false}
         renderItem={({ item: company }) => (
           <CompanyItem company={company} />
-        )}
-         />
+        )} />
     </View>
   );
 }

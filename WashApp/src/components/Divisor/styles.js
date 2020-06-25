60d074e064729428ac8a15e3db24f256a63f48b0
MrWilllian <willian.dev10@gmail.com>
2020-06-25 10:30:56 -0300

    style: add Divisor component style file

---
+++ WashApp/src/components/Divisor/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../assets/colors';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: colors.cinza2,
+    width: '100%',
+    height: '0.4%',
+    borderRadius: 10,
+    marginTop: 20,
+  },
+});

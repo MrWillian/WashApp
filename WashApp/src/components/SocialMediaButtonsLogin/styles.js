376aea302ae24fc5226f4e094b8c37cc9f814971
MrWilllian <willian.dev10@gmail.com>
2020-06-25 09:57:45 -0300

    style: add style for Buttons Container

---
+++ WashApp/src/components/SocialMediaButtonsLogin/styles.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import colors from '../../assets/colors';
+
+export default StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '20%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});

18c397bcc507a4667d404a57d9c111f349e5e6cb
MrWilllian <willian.dev10@gmail.com>
2020-06-25 10:31:12 -0300

    refactor: add Divisor component index file

---
+++ WashApp/src/components/Divisor/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import styles from './styles';
+
+export default function Divisor() {
+  return (
+    <View style={styles.container} />
+  );
+}
